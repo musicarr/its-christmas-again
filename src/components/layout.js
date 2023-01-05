e4c1c865cc18279e8f13fcda58d52260fd9d40c0
Mark Wheeler <markwheeler@marks-mini-2.lan>
2023-01-05 15:10:09 -0800

    First push with the target site content

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,56 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText
-} from './layout.module.css'
+import * as styles from './layout.module.css'
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ children }) => {
   return (
-    <div className={container}>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
-        {children}
-      </main>
-    </div>
+	<div className={styles.pageWrap}>
+		<main className={styles.mainWrap}>
+		{children}
+		</main>
+
+		<div className={styles.footerWrap}>
+			<div className={styles.reviewsWrap}>
+			reviews
+			</div>
+			<div className={styles.navBoilPadWrap}>
+				<nav className={styles.footerNav}>
+				<Link to="/privacy-policy">
+              		Privacy Policy
+            	</Link>
+            	<Link to="/privacy-policy">
+              		Terms &amp; Conditions
+            	</Link>
+            	<Link to="/how-this-works">
+              		How This Works
+            	</Link>
+            	<Link to="/compatibility">
+              		Device Compatibility
+            	</Link>
+            	<Link to="/playback-test">
+              		Video Playback
+            	</Link>
+				</nav>
+				<div className={styles.movieBoilerplate}>
+				movie boilerplate
+				</div>
+				<div className={styles.footerPadding}>
+				blank div padding
+				</div>
+			</div>
+		</div>
+
+		<nav className={styles.headerNavWrap}>
+		fixed header nav
+		</nav>
+	</div>
   )
 }
 
 export default Layout
+
+export const Head = () => (
+  <>
+    <link rel="preconnect" href="https://fonts.googleapis.com" /><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /><link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet" />
+  </>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,8 @@ import Layout from '../components/layout'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <Layout>
+      Main content goes here....
     </Layout>
   )
 }
