7b2cbc1fa16ed48557b4823b1acec999d4f6d763
Mark Wheeler <markwheeler@marks-mini-2.lan>
2023-01-05 11:29:31 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `It's Christmas Again`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
